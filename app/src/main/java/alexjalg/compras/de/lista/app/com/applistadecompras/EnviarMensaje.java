9d7ba46eb42bd2162a68a7f70a6eb5c44b26e504
Alejandro <alaura@3psi.com.pe>
2016-10-19 17:29:56 -0500

    18.- Configuración necesaria, se eliminaron los archivos del fragment

--- app/src/main/java/alexjalg/compras/de/lista/app/com/applistadecompras/EnviarMensaje.java
+++
@@ -1,9 +0,0 @@
-package alexjalg.compras.de.lista.app.com.applistadecompras;
-
-/**
- * Created by alejandro on 16/10/16.
- */
-
-public interface EnviarMensaje {
-    public void enviarDatos(String mensaje);
-}
---
+++ app/src/main/java/alexjalg/compras/de/lista/app/com/applistadecompras/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package alexjalg.compras.de.lista.app.com.applistadecompras.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexjalg.compras.de.lista.app.com.applistadecompras.R;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
